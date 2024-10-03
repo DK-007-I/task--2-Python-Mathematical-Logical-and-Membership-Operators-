418b027f7f79ce41ebb3f3838ab1cf3f81a1723e
DK-007-I <imartynov826@gmail.com>
2024-10-03 21:56:25 +0300

    Add files via upload

---
+++ -2-Python-Mathematical-Logical-and-Membership-Operators/task_2.py
@@ -0,0 +1,26 @@
+x = 7
+y = 3
+z = 0
+
+sum_result = x + y
+print("сумма x и y:", sum_result)
+
+difference_result = x - y
+print("разность x и y:", difference_result)
+
+product_result = x * y
+print("Умножение x на y:", product_result)
+
+remainder_result = x % y
+print("Деление x на y с остатком:", remainder_result)
+
+division_result = x // y
+print("Пеление x на y без остатка:", division_result)
+
+
+power_result = x ** y
+print("Возведение x в степень y:", power_result)
+
+ 
+z = x**2 - (x * y)
+print(" значение z:", z)
---
+++ -2-Python-Mathematical-Logical-and-Membership-Operators/task_5.py
@@ -0,0 +1,6 @@
+backpack = ['ручка', 'тетрадь', 'учебник', 'карандаш', 'фломастер', 'линейка', 'дастик']
+
+print('карандаш' in backpack)  # True
+
+print('яблоко' in backpack)   # False
+print(len(backpack))          # 7
